ea29d9f8e0ad65815730f7eec2e0f8fe901331db
Ho Silas Olive <hosilasolie@gmail.com>
2019-11-19 16:27:01 -0200

    Cobrinha andado

---
+++ empty-example/cobra.js
@@ -0,0 +1,12 @@
+function cobra() {
+    this.x = 0;
+    this.y = 0;
+    this.update = function(){
+        this.x += this.xvelocidade*tamanho;
+        this.y += this.yvelocidade*tamanho;
+    }
+    this.show = function(){
+        fill(255)
+        rect(this.x,this.y,tamanho,tamanho)
+    }
+}
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,15 @@
+const tamanho = 10;
+
+
+
 function setup() {
   createCanvas(400,400);
+   cobrinha = new cobra();
+   frameRate();
 }
 
 function draw() {
   background(51)
+  cobrinha.show();
+  cobrinha.update();
 }
