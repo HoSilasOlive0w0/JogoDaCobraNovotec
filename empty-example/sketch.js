d7e5f222044a7a7bd6d065df008a098c4072f6e6
Ho Silas Olive <hosilasolie@gmail.com>
2019-11-19 16:57:43 -0200

    Cobrinha ligeira

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,15 +1,28 @@
 const tamanho = 10;
+let cobrinha;
 
 
 
 function setup() {
-  createCanvas(400,400);
-   cobrinha = new cobra();
-   frameRate();
+  createCanvas(400, 400);
+  cobrinha = new cobra();
+  frameRate(100);
 }
 
 function draw() {
   background(51)
   cobrinha.show();
   cobrinha.update();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    cobrinha.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    cobrinha.dir(0, 1);
+  } else if (keyCode === LEFT_ARROW) {
+    cobrinha.dir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    cobrinha.dir(1, 0);
+  }
 }
